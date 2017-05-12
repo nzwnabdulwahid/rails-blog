f8822a223ca132cd0ea8bf49426b7b50abb00330
Niezwan <nzwnabdulwahid@gmail.com>
2017-05-12 22:26:56 +0800

    edit mod lesson 13

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -4,4 +4,23 @@ class Admin::ModeratorsController < ApplicationController
 		@moderators = Moderator.all
 	end
 
+	def edit
+		@moderator = Moderator.find(params[:id])
+	end
+
+	def update
+		@moderator = Moderator.find(params[:id])
+		if @moderator.update(moderator_params)
+			redirect_to :back
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def moderator_params
+		params.require(:moderator).permit(:id,:username,:fullname,:password)
+	end
+
 end
